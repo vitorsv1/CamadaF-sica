1955556453c361d981fbd77d79b548e1ba3296e3
Mateus Enrico Caruso <mateusenrico.mec@gmail.com>
2018-08-23 23:05:49 -0300

    Atualizacao diaria

---
+++ Projeto 3/desempacota.py
@@ -0,0 +1,7 @@
+# LER 65536 BITS!!!!!!!!!!!!!!!!!!!! O tamanho reflete na representacao em hexa, mas nao na contagem necessaria
+
+def main(dado):
+    
+
+if __name__ == "__main__":
+    main()
--- Projeto 3/empacota.py
+++ Projeto 3/empacota.py
@@ -1,9 +1,9 @@
 import math
 
-def main(dado):
+def geral(dado):
     tipoEncode = "utf-8"
     sizeInteiro = len(dado)
-    maxSize = 255 # 8 bits pra representar o tamanho do payload
+    maxSize = 65535 # 16 bits pra representar o tamanho do payload
 
     number = math.ceil(sizeInteiro/maxSize)
     count = number
@@ -18,41 +18,55 @@ def main(dado):
         atual = number - count
 
         # PAYLOAD
-        if atual != number:
-            if atual == 0:
-                adendo = 0
-            else:
-                adendo = 1
-            carga = dado[(maxSize*atual+adendo):(maxSize*(atual+1))]
-            size = maxSize
+        if sizeInteiro <= maxSize:
+            size = sizeInteiro
+            carga = dado[0:]
         else:
-            carga = dado[(maxSize*atual+adendo):]
-            size = sizeInteiro - (number - 1)*maxSize
+            if count == 1:
+                size = sizeInteiro - (number - 1)*maxSize
+                carga = dado[(maxSize*atual+1):]
+            else:
+                if atual == 0:
+                    adendo = 0
+                else:
+                    adendo = 1
+
+                size = maxSize
+                carga = dado[(maxSize*atual+adendo):(maxSize*(atual+1))]
+
 
         # HEAD
-        tamanho = size
+        #So foi dado extend
 
         # EOP
-        info = 'final'
+        primeiro = 255
+        segundo = 254
+        terceiro = 253
+        quarto = 252
 
         # MONTANDO #
-        head.extend(tamanho.to_bytes(1,'big')) 
+        head.extend(size.to_bytes(2,'big')) 
         head.extend(atual.to_bytes(1,'big'))
-        head.extend(number.to_bytes(1,'big'))
+        head.extend((number-1).to_bytes(1,'big'))
 
         pay.extend(bytes(carga))
 
-        eop.extend(bytes(info,tipoEncode))
+        eop.extend(primeiro.to_bytes(1,'big'))
+        eop.extend(segundo.to_bytes(1,'big'))
+        eop.extend(terceiro.to_bytes(1,'big'))
+        eop.extend(quarto.to_bytes(1,'big'))
 
         msg.extend(head)
         msg.extend(pay)
         msg.extend(eop)
         # ADICIONA A VARIAVEL PARA O BUFFER
         envio.extend(msg)
-
+        
         count = count - 1
-    
+    print(envio)
     return(envio)
 
 if __name__ == "__main__":
-    main()
+    imgLida = "C:/Users/Mateus Enrico/Documents/Insper/CamadaFisica/Projeto 3/img/x.png"
+    img = open(imgLida,'rb')
+    geral(img.read())
