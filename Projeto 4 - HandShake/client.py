7de3a767ae14a43dfb2bf687d61bbf3d6aeab9a7
vitorsv1 <vitorsv1@al.insper.edu.br>
2018-08-30 15:22:54 -0300

    Tirei alguns comentarios

--- Projeto 4 - HandShake/client.py
+++ Projeto 4 - HandShake/client.py
@@ -10,24 +10,15 @@
 
 print("comecou")
 
-#aokdaopskdp
-
 from enlace import *
 import time
 from tkinter import filedialog, ttk
 from tkinter import *
 from tkinter.filedialog import askopenfilename
- 
-# voce deverá descomentar e configurar a porta com através da qual ira fazer a
-# comunicaçao
-# Serial Com Port
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-# se estiver usando windows, o gerenciador de dispositivos informa a porta
 
-serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-#serialName = "COM4"                  # Windows(variacao de)
+serialName = "COM4"                  # Windows(variacao de)
 
 
 def main():
--- Projeto 4 - HandShake/server.py
+++ Projeto 4 - HandShake/server.py
@@ -15,13 +15,6 @@ print("comecou")
 from enlace import *
 import time
 
-# voce deverá descomentar e configurar a porta com através da qual ira fazer a
-# comunicaçao
-# Serial Com Port
-#   para saber a sua porta, execute no terminal :
-#   python -m serial.tools.list_ports
-# se estiver usando windows, o gerenciador de dispositivos informa a porta
-
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
 serialName = "COM7"                  # Windows(variacao de)
@@ -41,10 +34,6 @@ def main():
     #imgEscrita = "C:/Users/Mateus Enrico/Documents/Insper/CamadaFisica/Projeto 3/img/recebido.png"
     imgEscrita = "C:/Users/vitor/Dropbox/Insper/2018.2/Camada Física/CamadaFisica/Projeto 3/img/recebido.png"
     
-    # a seguir ha um exemplo de dados sendo carregado para transmissao
-    # voce pode criar o seu carregando os dados de uma imagem. Tente descobrir
-    #como fazer isso
-
     # Faz a recepção dos dados
     print ("Recebendo dados .... ")
     
