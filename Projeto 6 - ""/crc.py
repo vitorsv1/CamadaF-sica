7405a310ba4a6dfc39a65df0bbed9e9b8a4170ba
Mateus Enrico <mateusenrico@localhost.localdomain>
2018-09-21 16:14:40 -0300

    zd

---
+++ Projeto 6 - ""/crc.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+from PyCRC.CRC16 import CRC16
+from PyCRC.CRC16DNP import CRC16DNP
+from PyCRC.CRC16Kermit import CRC16Kermit
+from PyCRC.CRC32 import CRC32
+from PyCRC.CRCCCITT import CRCCCITT
+
+data = '0xF3'
+dado = '123'
+print(CRCCCITT().calculate(data))
