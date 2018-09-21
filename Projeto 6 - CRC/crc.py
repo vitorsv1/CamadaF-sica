1565fe7018afea9f7d500264b590b0a266f9b27f
mateusenrico <mateusesrec@al.insper.edu.br>
2018-09-21 15:57:27 -0300

    asd

--- Projeto 6 - CRC/crc.py
+++
@@ -1,9 +0,0 @@
-from PyCRC.CRC16 import CRC16
-from PyCRC.CRC16DNP import CRC16DNP
-from PyCRC.CRC16Kermit import CRC16Kermit
-from PyCRC.CRC16SICK import CRC16SICK
-from PyCRC.CRC32 import CRC32
-from PyCRC.CRCCCITT import CRCCCITT
-
-input = '12345'
-print(CRCCCITT().calculate(input))
