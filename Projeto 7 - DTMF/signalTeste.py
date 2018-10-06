273a60d21a2ccb78d19031d3847f7cf6c764434d
vitorsv1 <vitorsv1@al.insper.edu.br>
2018-10-06 14:26:35 -0300

    Dale

--- Projeto 7 - DTMF/signalTeste.py
+++ Projeto 7 - DTMF/signalTeste.py
@@ -37,18 +37,23 @@ class signalMeu:
 if __name__ == "__main__":
     SM = signalMeu()
     penis = []
+    loop = True
     numbers = [[941, 1336],[697,1209],[697, 1336],[697, 1477],[770, 1209],[770, 1336],[770, 1477],[852, 1209],[852, 1336],[852, 1447]]
     sd.default.samplerate =  44100
-    while True:
-        n = input("Qual numero? ")
-        x = numbers[int(n)] #Pegar as duas frequencias relativas ao valor
-        #print(x[0])
-        x1,s1= SM.generateSin(x[0],10,0.35,44100)
-        x1,s2= SM.generateSin(x[1],10,0.35,44100)
-        s = []
-        for i in range(len(s1)):
-            s.append(s1[i]+s2[i])
-        sd.play(s) #Tocar as duas frequencias juntas
-        sd.wait()
+    while loop:
+        n = input("Qual numero? (Digite s para sair): ")
+        if (n == 's'):
+            loop = False
+            break
+        else:
+            x = numbers[int(n)] #Pegar as duas frequencias relativas ao valor
+            #print(x[0])
+            x1,s1= SM.generateSin(x[0],10,0.35,44100)
+            x1,s2= SM.generateSin(x[1],10,0.35,44100)
+            s = []
+            for i in range(len(s1)):
+                s.append(s1[i]+s2[i])
+            sd.play(s) #Tocar as duas frequencias juntas
+            sd.wait()
     
     print('sugma')
