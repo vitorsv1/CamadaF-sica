f16074f0b2a391c8b7106b03c0be1deaf3791732
vitorsv1 <vitorsv1@al.insper.edu.br>
2018-10-26 10:17:46 -0300

    receptor

--- Projeto 8 - AM/receptor.py
+++ Projeto 8 - AM/receptor.py
@@ -4,22 +4,21 @@ import soundfile as sf
 import matplotlib.pyplot as plt
 from scipy.signal import kaiserord, lfilter, firwin, freqz
 
+duration = 5
+fs = 44100
+freqC = 9000
+SM = st.signalMeu()
+
 while True:
         print("start")
-        myrecording = sd.rec(myarray, samplerate=fs, channels=1)
+        myrecording = sd.rec(int(duration*fs), samplerate=fs, channels=1)
         sd.wait()
-        #print((myrecording))
-        #sd.play(myrecording) 
-        #sd.wait()
+        
+        sd.play(myrecording) 
+        sd.wait()
         s = []
         for i in myrecording:
             s.append(i[0])
-        peaks = SM.getFFT(s,44100)
+        print(s)
+        x1,s1 = SM.generateSin(freqC,1,len(s)/44100, 44100)
         
-        for i in range(len(peaks)-1):
-            for j in freq_dict:
-                #if peaks[i]==j[0] and peaks[i+1]==j[1]:
-                z = freq_dict[j]
-                if math.isclose(peaks[i], z[0], rel_tol=0) and math.isclose(peaks[i+1], z[1], rel_tol=0):
-                    print("Tecla apertada " + j)
-                    SM.plotFFT(s,44100)
